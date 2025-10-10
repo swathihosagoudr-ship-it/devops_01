ee717c4b323bc1d0b64e550437e4202819fa8f62
swathihosagpudr <swathihosagoudr@gamil.com>
2025-10-10 16:06:35 +0530

    saving #rd journal program

---
+++ program03.py
@@ -0,0 +1,12 @@
+num1 = input ("enter frist number:")
+num2 = input("enter second number:")
+
+print("\Before swapping:")
+print("frist number = ", num1)
+print("second number =" , num2)
+
+num1 , num2 = num2 , num1
+
+print("\After swapping:")
+print("frist number = ", num1)
+print("second number =" , num2)
